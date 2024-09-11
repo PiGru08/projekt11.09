a18226f80b3860a088ef4b1c5604df7a8beb6b4a
PiGru08 <grucar77@gmail.com>
2024-09-11 12:24:24 +0200

    Kolekcja lista

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //tablice
@@ -16,6 +20,27 @@ public class Main {
         for (int element:tablicaLosowych) {
             System.out.print(element+", ");
         }
+        //kolekcje, elementy typami złożonymi
+        //nie muszą mieć zdefiniowanego rozmiaru
+        //rozmiar w trakcie może się zmieniać
+        //listy, zbiory Set, mapy
 
+        //wstawianie liczb z klatwiatury do kolekcji
+
+        List<Integer> listaLiczbWpisanych = new ArrayList<>();
+        System.out.println("Podaj 6 liczb:");
+        Scanner klawiatura = new Scanner(System.in);
+        for (int i = 0; i < 6; i++) {
+            int liczba = klawiatura.nextInt();
+            listaLiczbWpisanych.add(liczba);
+        }
+        System.out.println("Wstawione liczby: ");
+            for (Integer liczbaWpisana:listaLiczbWpisanych) {
+                System.out.println(liczbaWpisana);
+            }
+            //wypisywanie innym sposobem
+        for (int i = 0; i <listaLiczbWpisanych.size() ; i++) {
+            System.out.println(listaLiczbWpisanych.get(i));
+        }
     }
 }
