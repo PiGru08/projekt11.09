dc67bf207ce322552630ce1cd7b134c349102f1d
PiGru08 <grucar77@gmail.com>
2024-09-11 13:10:59 +0200

    Sprawdzanie liczb trafionych w licie i wypisywanie

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -79,5 +76,15 @@ public class Main {
         Elementy są unikatowe
         Elementy nie są indeksowane
          */
+
+        LinkedList<Integer> trafione = new LinkedList<>();
+        //trafione to elementy które występują w wylosowanych i wpisanych
+
+        for (Integer wpisana: listaLiczbWpisanych) {
+            if(zbiorLiczbLosowych.contains(wpisana)){
+                trafione.add(wpisana);
+            }
+        }
+        System.out.println("Trafione: "+trafione);
     }
 }
